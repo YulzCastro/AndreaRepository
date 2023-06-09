4cd4c75ee709dabde73b42906599ceb71d218c7f
YulzCastro <andreayulitzacastrogomez88@gmail.com>
2023-06-09 13:41:18 -0500

    Apuntes de clase

---
+++ Funciones/script.js
@@ -0,0 +1,5 @@
+const alumnos = ['Anderson', 'Jeffry', 'Andrés', 'Steven', 'William', 'José Fernando'];
+
+for(let index = 0; index < alumnos.length; index++) {
+    
+}
--- Intro JS/script.js
+++ Intro JS/script.js
@@ -1,7 +1,11 @@
-console.log("Hola mundo");
+/*console.log("Hola mundo");
 
 document.write("Hola mundo x2");
 
 alert("Hola Mundo x3");
 
 var mensaje=("Espero que les esté gustando JS");
+*/
+
+HTML & JS
+
